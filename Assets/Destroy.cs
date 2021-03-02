2bff981e464cab01e3ce427706d914c1e40cee1f
yash1shi <78535052+yash1shi@users.noreply.github.com>
2021-03-02 20:30:02 +0900

    Add files via upload

---
+++ Assets/Destroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Destroy : MonoBehaviour
+{
+    public Button btn2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    void bye()
+    {
+        this.gameObject.SetActive(false);
+        btn2.interactable = true;
+    }
+}
---
+++ Assets/push.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class push : MonoBehaviour
+{
+
+    public GameObject img;
+    public Button btn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn.interactable = true;
+        img.SetActive(false);
+    }
+
+    public void Button_press()
+    {
+        img.SetActive(true);
+        btn.interactable = false;
+        Invoke("bye", 3);
+    }
+
+    void bye()
+    {
+        img.SetActive(false);
+        btn.interactable = true;
+    }
+
+}
